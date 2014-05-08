a3147f755e76d439d3e5cdefcfc11250ff544c28
Ben Keen <ben.keen@gmail.com>
2014-05-07 23:11:08 -0700

    2.0.0-beta-20091122

--- global/init_page.php
+++ global/init_page.php
@@ -3,12 +3,23 @@
 // our Smarty instance, used for rendering the webpages
 $g_smarty = new Smarty();
 
-// if the config file exists, we can assume the user isn't installed
-$g_link = ft_db_connect();
+$config_file_exists = false;
+if (is_file("$folder/config.php"))
+{
+  $folder = dirname(__FILE__);
+  $config_file_exists = true;
+  include_once("$folder/config.php");
+}
 
-// load the appropriate language file
-$g_language = ft_get_ui_language();
-require_once("$folder/lang/{$g_language}.php");
+if ($config_file_exists)
+{
+  // if the config file exists, we can assume the user isn't installed
+  $g_link = ft_db_connect();
 
-if (isset($_GET["logout"]))
-  ft_logout_user();
+  // load the appropriate language file
+  $g_language = ft_get_ui_language();
+  require_once("$folder/lang/{$g_language}.php");
+
+  if (isset($_GET["logout"]))
+    ft_logout_user();
+}
