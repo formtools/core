89d82b2ba6980bfa4954b81fc361ea11656679cb
Ben Keen <ben.keen@gmail.com>
2017-11-07 22:00:42 -0800

    resetting sessions for fresh installs

--- install/index.php
+++ install/index.php
@@ -9,9 +9,15 @@ require_once("../global/library.php");
 use FormTools\Core;
 use FormTools\General;
 use FormTools\Installation;
+use FormTools\Sessions;
 
 Core::setHooksEnabled(false);
 Core::startSessions();
+
+// note we reset sessions here. This prevents weird things happening when a new installation is taking place
+// while an old orphaned FT session exists, containing who-knows-what
+Sessions::clearAll();
+
 Core::initSmarty();
 Core::setCurrLang(General::loadField("lang_file", "lang_file", Core::getDefaultLang()));
 
