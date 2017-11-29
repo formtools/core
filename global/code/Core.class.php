c7f774c7baee4175c7077b463ed8389c06252b36
Ben Keen <ben.keen@gmail.com>
2017-11-28 19:56:02 -0800

    3.0.0-alpha-20171128

--- global/code/Core.class.php
+++ global/code/Core.class.php
@@ -256,7 +256,7 @@ class Core {
     /**
      * The release date: YYYYMMDD
      */
-    private static $releaseDate = "20171127";
+    private static $releaseDate = "20171128";
 
     /**
      * The minimum required PHP version needed to run Form Tools.
