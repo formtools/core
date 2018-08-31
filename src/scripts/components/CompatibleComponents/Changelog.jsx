d2965eea06984c11b0e8d1c9f9032c2a0a8304ce
Ben Keen <ben.keen@gmail.com>
2018-08-31 15:52:45 -0700

    component milestone links (incomplete)

--- src/scripts/components/CompatibleComponents/Changelog.jsx
+++ src/scripts/components/CompatibleComponents/Changelog.jsx
@@ -2,37 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Changelog.scss';
 import { formatDatetime } from '../../core/helpers';
-// import IconButton from '@material-ui/core/IconButton';
-// import DeleteIcon from '@material-ui/icons/Delete';
+import { Github } from '../Icons/Icons';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
 import VersionBadge from './VersionBadge';
 
 
-const Changelog = ({ data }) => (
-    <table className={styles.changelog}>
-        <tbody>
-        <tr>
-            <th className={styles.colVersion}>Version</th>
-            <th className={styles.colReleaseDate}>Release Date</th>
-            <th>Release Notes</th>
-            <th className={styles.colGithubMilestone} />
-        </tr>
-        {data.map(({ version, release_date, desc }, i) => (
-            <tr key={i}>
-                <td className={styles.colVersion}>
-                    <VersionBadge label={version} />
-                </td>
-                <td className={styles.colReleaseDate}>{formatDatetime(release_date)}</td>
-                <td>{desc}</td>
-                <td>
-                    {/*<IconButton aria-label="Delete">*/}
-                        {/*<DeleteIcon />*/}
-                    {/*</IconButton>*/}
-                </td>
+const Changelog = ({ data, i18n }) => {
+
+    const getGithubIcon = (githubMilestone) => {
+        if (!githubMilestone) {
+            return null;
+        }
+        return (
+            <Tooltip title="View full changes on github" placement="left">
+                <IconButton className={styles.githubIcon} href={githubMilestone}>
+                    <Github color="#999999"/>
+                </IconButton>
+            </Tooltip>
+        );
+    };
+
+    return (
+        <table className={styles.changelog}>
+            <tbody>
+            <tr>
+                <th className={styles.colVersion}>Version</th>
+                <th className={styles.colReleaseDate}>Release Date</th>
+                <th className={styles.colDesc}>Release Notes</th>
             </tr>
-        ))}
-        </tbody>
-    </table>
-);
+            {data.map(({version, release_date, desc}, i) => (
+                <tr key={i}>
+                    <td className={styles.colVersion}>
+                        <VersionBadge label={version}/>
+                    </td>
+                    <td className={styles.colReleaseDate}>{formatDatetime(release_date)}</td>
+                    <td className={styles.colDesc}>
+                        {desc}
+                        {getGithubIcon(data.github_milestone)}
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
+};
 
 Changelog.propTypes = {
     data: PropTypes.array
