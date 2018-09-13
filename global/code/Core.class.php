e544cf777e69993d29b710bd3d2b1945af252a7e
Ben Keen <ben.keen@gmail.com>
2018-09-13 16:59:14 -0700

    3.0.7

--- global/code/Core.class.php
+++ global/code/Core.class.php
@@ -253,7 +253,7 @@ class Core
 	/**
 	 * The current version of the Form Tools Core.
 	 */
-	private static $version = "3.0.6";
+	private static $version = "3.0.7";
 
 	/**
 	 * The release type: alpha, beta or main
@@ -263,7 +263,7 @@ class Core
 	/**
 	 * The release date: YYYYMMDD
 	 */
-	private static $releaseDate = "20180811";
+	private static $releaseDate = "20180913";
 
 	/**
 	 * The minimum required PHP version needed to run Form Tools.
