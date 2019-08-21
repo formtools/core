37099c261779f5168373442de18ea398d3639429
Ben Keen <ben.keen@gmail.com>
2019-08-20 22:13:22 -0700

    continued work on adding React

--- src/react/bundles/installation.js
+++ src/react/bundles/installation.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { actionCreators } from '../store/init';
-import InstallationPage from '../layout/InstallationPage';
+import PageContainer from '../containers/Page.container';
 import store from '../store';
 
 // boot 'er up. The initialization data is required on every page (i18n, user info, etc)
 actionCreators.getInitializationData(store);
 
 const App = () => (
-	<InstallationPage>
+	<PageContainer>
 		<div>Content here.</div>
-	</InstallationPage>
+	</PageContainer>
 );
 
 ReactDOM.render(
--- src/react/containers/Page.container.js
+++ src/react/containers/Page.container.js
@@ -1,2 +1,34 @@
 import React, { Component } from 'react';
+import { Provider, connect } from 'react-redux';
+import store from '../store';
+import { selectors as initSelectors } from '../store/init';
+import { selectors as i18nSelectors } from '../store/i18n';
+import Page from '../containers/Page.container';
 
+class PageContainer extends Component {
+	componentWillUpdate (nextProps) {
+		if (nextProps.initialized && !this.props.initialized) {
+			this.props.getInstallationComponentList();
+		}
+	}
+	render () {
+		return (
+			<Page {...this.props} />
+		);
+	}
+}
+
+const mapStateToProps = (state) => ({
+	initialized: initSelectors.getInitialized(state),
+	i18n: i18nSelectors.getI18n(state),
+});
+
+const ConnectedPageContainer = connect(
+	mapStateToProps
+)(PageContainer);
+
+export default (
+	<Provider store={store}>
+		<ConnectedPageContainer />
+	</Provider>
+);
